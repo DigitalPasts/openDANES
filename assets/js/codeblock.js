39efc24a2d002b0c17a7c27eaed75cdd5e817865
DigitalPasts <digpasts@gmail.com>
2023-04-07 01:43:19 -0400

    add style to paper

---
+++ assets/js/codeblock.js
@@ -0,0 +1,24 @@
+var codeBlocks = document.querySelectorAll('pre.highlight');
+
+codeBlocks.forEach(function (codeBlock) {
+  var copyButton = document.createElement('button');
+  copyButton.className = 'copy';
+  copyButton.type = 'button';
+  copyButton.ariaLabel = 'Copy code to clipboard';
+  copyButton.innerHTML = '<span class="code-button-emoticon">\uf0c5</span> Copy me';
+
+  codeBlock.before(copyButton);
+
+  copyButton.addEventListener('click', function () {
+    var code = codeBlock.querySelector('code').innerText.trim();
+    window.navigator.clipboard.writeText(code);
+
+    copyButton.innerHTML = '<span class="code-button-emoticon">\uf00c</span> Copied!';
+    var fourSeconds = 2000;
+
+    setTimeout(function () {
+      copyButton.innerHTML = '<span class="code-button-emoticon">\uf0c5</span> Copy me';
+      
+    }, fourSeconds);
+  });
+});
